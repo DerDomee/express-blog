da9763bdb5ba6c5c01dffc3378baaa81e92d35d4
Dominik Riedig <me@dominikriedig.de>
2022-05-29 23:18:38 +0200

    dev: setup proper testing environment

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-	transform: {'^.+\\.ts?$': 'ts-jest'},
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+	verbose: true,
 	testEnvironment: 'node',
-	// testRegex: '.(test|spec)?`\\.(ts|tsx)$',
+	transform: {
+		'^.+\\.ts?$': 'ts-jest',
+	},
+	testRegex: '.(test|spec).(ts|ts)$',
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
+
+module.exports = config;
