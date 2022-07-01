f2520fd0422b25e6947e2e8ac904254cfce8e010
Dominik Riedig <me@dominikriedig.de>
2022-07-01 19:34:52 +0200

    fix(test): Fix failing test for cloudcenter routes
    Erroneous access to higher scope variable in unit test
    context lets the test fail

--- src/cloudcenter/routes/tvShowBrowser.ts
+++ src/cloudcenter/routes/tvShowBrowser.ts
@@ -1,7 +1,6 @@
 import {NextFunction, Request, Response} from 'express';
 import TvShow from '../../database/dbmodels/tvshow.model';
 import {Route} from '../../mean/types';
-import app from '../cloudcenter';
 
 /**
  *
@@ -10,7 +9,7 @@ import app from '../cloudcenter';
  * @param {NextFunction} next
  */
 async function get(req: Request, res: Response, next: NextFunction) {
-	app.locals.allTvShows = await TvShow.findAll();
+	res.locals.allTvShows = await TvShow.findAll();
 	res.locals.pageTitle = 'Serienbrowser';
 	res.locals.htmlTitle = 'Serien - Cloudcenter - Dominik Riedig';
 	res.render('tvShowBrowser', {...req.app.locals, ...res.locals});
