b94737873cb927d0b9da318150f6b4494c18c0d4
Dominik Riedig <me@dominikriedig.de>
2022-07-05 17:58:59 +0200

    refactor: better structure in dynamicImages

--- src/mean/routes/dynamicimages.ts
+++ src/mean/routes/dynamicimages.ts
@@ -3,6 +3,42 @@ import {Route} from '../types';
 import fs from 'fs';
 import * as PImage from 'pureimage';
 
+const fileExtToMimeSubtype = {
+	bmp: 'bmp',
+	svg: 'svg+xml',
+	png: 'png',
+	jpeg: 'jpeg',
+	jpg: 'jpeg',
+	avif: 'avif',
+	gif: 'gif',
+	webp: 'webp',
+};
+
+
+const createPlaceholderImage = async (
+	pictureid: string,
+	width: number,
+	height: number,
+) => {
+	const image = PImage.make(width, height, {});
+	const ctx = image.getContext('2d');
+	ctx.fillStyle = 'red';
+	ctx.fillRect(0, 0, width, height);
+	ctx.fillStyle = 'green';
+	ctx.strokeStyle = 'green';
+	/* ctx.fillRect(0, 0, width, 3);
+		ctx.fillRect(0, 0, 3, height);
+		ctx.fillRect(width-3, 0, width, height);
+		ctx.fillRect(0, height-3, width, height);*/
+	ctx.lineWidth = 15;
+	ctx.strokeRect(0, 0, width - 4, height - 4);
+	await fs.promises.mkdir('data/images', {recursive: true});
+	await PImage.encodeJPEGToStream(
+		image,
+		fs.createWriteStream(`data/images/${pictureid}.jpeg`),
+	);
+};
+
 /**
  *
  * @param {Request} req
@@ -10,49 +46,20 @@ import * as PImage from 'pureimage';
  * @param {NextFunction} next
  */
 async function get(req: Request, res: Response, next: NextFunction) {
-	// Supported filetypes
-	const fileExtToMimeSubtype = {
-		bmp: 'bmp',
-		svg: 'svg+xml',
-		png: 'png',
-		jpeg: 'jpeg',
-		jpg: 'jpeg',
-		avif: 'avif',
-		gif: 'gif',
-		webp: 'webp',
-	};
-
 	const pictureid = req.params.pictureid;
 	const type = req.params.type as keyof typeof fileExtToMimeSubtype;
 	const mimetype = fileExtToMimeSubtype[type];
+	const queryWidth: number = parseInt(`${req.query.w}`);
+	const queryHeight: number = parseInt(`${req.query.h}`);
+
+	const outputWidth = isNaN(queryWidth) ? 400 : queryWidth;
+	const outputHeight = isNaN(queryHeight) ? 300 : queryHeight;
 
 	if (mimetype == undefined) {
 		res.status(415);
 		res.end();
 		return;
 	}
-
-	const createPlaceholderImage = async () => {
-		const width = 400;
-		const height = 300;
-		const image = PImage.make(width, height, {});
-		const ctx = image.getContext('2d');
-		ctx.fillStyle = 'red';
-		ctx.fillRect(0, 0, width, height);
-		ctx.fillStyle = 'green';
-		ctx.strokeStyle = 'green';
-		/* ctx.fillRect(0, 0, width, 3);
-		ctx.fillRect(0, 0, 3, height);
-		ctx.fillRect(width-3, 0, width, height);
-		ctx.fillRect(0, height-3, width, height);*/
-		ctx.lineWidth = 15;
-		ctx.strokeRect(0, 0, width - 4, height - 4);
-		await fs.promises.mkdir('data/images', {recursive: true});
-		await PImage.encodeJPEGToStream(
-			image,
-			fs.createWriteStream(`data/images/${pictureid}.jpeg`),
-		);
-	};
 
 	try {
 		const image = await fs.promises.readFile(
@@ -62,7 +69,7 @@ async function get(req: Request, res: Response, next: NextFunction) {
 		res.end(image);
 	} catch (err) {
 		if (type === 'jpeg') {
-			await createPlaceholderImage();
+			await createPlaceholderImage(pictureid, outputWidth, outputHeight);
 			const image = await fs.promises.readFile(
 				`data/images/${pictureid}.${type}`,
 			);
