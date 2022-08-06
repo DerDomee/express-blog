ab1330490737f4dbe49cb6bf0ce19b64d5b2fe04
Dominik Riedig <me@dominikriedig.de>
2022-08-06 07:40:33 +0200

    chore(lint): Add new rules for plugin:node

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
 	},
 	'extends': [
 		'google',
+		'plugin:node/recommended',
 	],
 	'parser': '@typescript-eslint/parser',
 	'parserOptions': {
@@ -17,14 +18,30 @@ module.exports = {
 	],
 	'rules': {
 		'comma-dangle': ['error', 'always-multiline'],
-		'comma-spacing': ['error', {before: false, after: true}],
+		'comma-spacing': ['error', {
+			before: false,
+			after: true,
+		}],
 		'comma-style': ['error', 'last'],
 		'indent': ['error', 'tab'],
-		'new-cap': ['error', {properties: false}],
+		'new-cap': ['error', {
+			properties: false,
+		}],
 		'no-tabs': 0,
 		'no-mixed-spaces-and-tabs': [2, 'smart-tabs'],
 		'new-cap': ['error', {
 			'capIsNew': false,
 		}],
+
+		'node/no-process-env': 'error',
+		'node/no-unsupported-features/es-syntax': ['error', {
+			'ignores': ['modules'],
+		}],
+		'node/no-missing-import': ['error', {
+			'allowModules': [],
+			'resolvePaths': [],
+			'tryExtensions': ['.js', '.ts'],
+		}],
+		'node/no-unpublished-import': 'error',
 	},
 };
