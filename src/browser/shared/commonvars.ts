247d2201407e8d16218cd4d8c0613537b3f303f4
Dominik Riedig <me@dominikriedig.de>
2022-12-27 16:04:59 +0100

    feat(browser): Add window namespace 'derdomee'

--- src/browser/shared/commonvars.ts
+++ src/browser/shared/commonvars.ts
@@ -1,1 +1,6 @@
 export const route = location.pathname.replaceAll(/(?=\/$)\//g, '');
+declare global {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	interface Window { derdomee: any; }
+}
+window.derdomee = window.derdomee || {};
