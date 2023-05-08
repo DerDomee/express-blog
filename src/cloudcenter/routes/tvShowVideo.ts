7e53839fee97acfe03ed39d9390c0f992a0e91aa
Dominik Riedig <me@dominikriedig.de>
2023-05-08 21:29:40 +0200

    feat(cloudcenter): Add route handling for nextEpisode

--- src/cloudcenter/routes/tvShowVideo.ts
+++ src/cloudcenter/routes/tvShowVideo.ts
@@ -13,6 +13,10 @@ async function get(req: Request, res: Response, next: NextFunction) {
 		where: {
 			episodeId: req.params.episodeId,
 		},
+		include: [{
+			all: true,
+			nested: true,
+		}],
 	});
 	if (!episode) {
 		res.redirect(`/tv/${req.params.showId}/?s=${req.params.seasonId}`);
@@ -20,10 +24,32 @@ async function get(req: Request, res: Response, next: NextFunction) {
 	res.locals.htmlTitle = `${episode.name} - Dominik Riedig`;
 	res.locals.backlink = `/tv/${req.params.showId}/?s=${req.params.seasonId}`;
 	res.locals.videoname = `${req.params.episodeId}.mp4`;
+	res.locals.episodename = episode.name;
+	res.locals.nextEpisode = episode.nextEpisode;
 	res.render('videoViewer', {...req.app.locals, ...res.locals});
+}
+
+/**
+ *
+ * @param {Request} req
+ * @param {Reponse} res
+ * @param {NextFunction} next
+ */
+async function post(req: Request, res: Response, next: NextFunction) {
+	const episode = await TvEpisode.findOne({
+		where: {
+			episodeId: req.params.episodeId,
+		},
+		include: [{
+			all: true,
+			nested: true,
+		}],
+	});
+	res.json(JSON.stringify(episode.nextEpisode));
 }
 
 export default {
 	routeMatcher: '/watch/:showId/:seasonId/:episodeId',
 	get: get,
+	post: post,
 } as Route;
